d7962ad8a4a07c49258a74aa8a4145ca20b83fdf
Oomat <kubanychbekovomat@gmail.com>
2022-12-13 20:24:09 +0600

    hw1_remake

--- shop/urls.py
+++ shop/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import hello_world_view, datetime_view, goodbye_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_world_view),
+    path('now_time/', datetime_view),
+    path('goodbye/', goodbye_view),
 ]
